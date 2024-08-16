57fed2646e4fd8c36ad810d1a282ed243f1c6cf7
Peter <redfoxgaming67@gmail.com>
2024-08-16 17:46:15 -0400

    Initial Commit

---
+++ src/main/java/net/redfox/tleveling/TinkersLeveling.java
@@ -0,0 +1,36 @@
+package net.redfox.tleveling;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(TinkersLeveling.MOD_ID)
+public class TinkersLeveling {
+	public static final String MOD_ID = "tleveling";
+	private static final Logger LOGGER = LogUtils.getLogger();
+
+	public TinkersLeveling() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::commonSetup);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void commonSetup(final FMLCommonSetupEvent event) {
+
+	}
+
+	@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientModEvents {
+		@SubscribeEvent
+		public static void onClientSetup(FMLClientSetupEvent event) {
+
+		}
+	}
+}
