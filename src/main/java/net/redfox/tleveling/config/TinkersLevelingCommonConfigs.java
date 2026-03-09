597a572f9bccdef64670a1255dcf4c04a8834895
Peter <85247098+PeterLangendorf@users.noreply.github.com>
2026-03-09 09:26:21 -0400

    Update common configs

--- src/main/java/net/redfox/tleveling/config/TinkersLevelingCommonConfigs.java
+++ src/main/java/net/redfox/tleveling/config/TinkersLevelingCommonConfigs.java
@@ -13,23 +13,23 @@ public class TinkersLevelingCommonConfigs {
   public static final ForgeConfigSpec.ConfigValue<Integer> LEVELUP_EXP_REQUIRED;
   public static final ForgeConfigSpec.ConfigValue<Double> LEVELUP_INCREASE;
 
-//  public static final ForgeConfigSpec.ConfigValue<Integer> LEVEL_BONUS_MODIFIER;
+  public static final ForgeConfigSpec.ConfigValue<Integer> LEVEL_BONUS_MODIFIER;
 
   static {
     BUILDER.push("Common Configs for Tinker's Tool Leveling");
 
     PICKAXE_EXP_MULTIPLIER = BUILDER.comment("The multiplier for the exp granted to a mining tool after each block break. Default is 1.0")
-        .define("Exp Per Block", 1.0d);
+        .defineInRange("blockBreakMultiplier", 1d, 0, Integer.MAX_VALUE);
     KILL_EXP_MULTIPLIER = BUILDER.comment("The multiplier for the exp granted to a melee weapon after each block break. Default is 1.0")
-        .define("Exp Per Melee Kill", 1.0d);
+        .defineInRange("damageEntityMultiplier", 1d, 0, Integer.MAX_VALUE);
     ARMOR_EXP_MULTIPLIER = BUILDER.comment("The multiplier for the exp granted to an armor piece after each block break. Default is 1.0")
-        .define("Exp Per Armor Hit", 1.0d);
-//    LEVEL_BONUS_MODIFIER = BUILDER.comment("The amount of levels between each bonus modifier. Set to 0 to disable bonus modifiers. Default is 3")
-//        .defineInRange("Levels Between Bonus Modifiers", 3, 0, 11);
+        .defineInRange("armorHitMultiplier", 1d, 0, Integer.MAX_VALUE);
+    LEVEL_BONUS_MODIFIER = BUILDER.comment("The amount of levels between each bonus modifier. Set to 0 to disable bonus modifiers. Default is 3")
+        .define("modifierLevelGap", 3);
     LEVELUP_EXP_REQUIRED = BUILDER.comment("The base exp required to level up a tool. Default is 500")
-        .defineInRange("Exp Required", 200, 1, 2147483647);
-    LEVELUP_INCREASE = BUILDER.comment("The exponential increase between tool exp levels")
-        .define("Exp Increase", 2d);
+        .defineInRange("levelupExpRequired", 200, 1, Integer.MAX_VALUE);
+    LEVELUP_INCREASE = BUILDER.comment("The exponential increase between tool exp levels. Default is 2.0")
+        .defineInRange("levelupExpIncrease", 2d, 0, Integer.MAX_VALUE);
 
     BUILDER.pop();
     SPEC = BUILDER.build();
